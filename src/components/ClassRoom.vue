5328492a0449f13b87a13dc15ef012dea665fe4a
kota-matsui-edi <matsui@ediplex.com>
2019-11-15 12:16:58 +0900

    set gltf loader

---
+++ src/components/ClassRoom.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="classroom">
+    <canvas></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+@Component
+export default class ClassRoom extends Vue {
+  @Prop() private msg!: string;
+  loader = new GLTFLoader()
+  created () {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
